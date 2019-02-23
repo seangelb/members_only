4c39b70d8ddda2574b56893e7e7532c9c4279faf
sean <smgelbgithub@gmail.com>
2019-02-23 15:54:55 -0500

    push once.seis this is so aggravating

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_195635) do
+ActiveRecord::Schema.define(version: 2019_02_23_205257) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
     t.text "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
