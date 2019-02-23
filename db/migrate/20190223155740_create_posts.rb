9993032e6e7af0e99b01f569bf1dfb696accdfe1
sean <smgelbgithub@gmail.com>
2019-02-23 15:13:20 -0500

    push once.tres - had to delete database and restart it. Never mess with database.

--- db/migrate/20190223155740_create_posts.rb
+++
@@ -1,5 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :title, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_155740) do
+ActiveRecord::Schema.define(version: 2019_02_23_195635) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "title"
-    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
