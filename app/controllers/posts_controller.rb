618b28ef6bdb8c7864a7ba436c9ed246be4b6bc8
sean <smgelbgithub@gmail.com>
2019-02-23 15:35:56 -0500

    push once.cinco

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,16 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
   
   def new
-    @posts = Post.new
+    @post = Post.new
   end
   
   def create
     Post.create(title: params[:post][:title], content: params[:post][:content], user_id: current_user.id)
+    redirect_to '/posts'
   end
   
   def index
-    @posts = Post.all
+    @post = Post.all
   end
   
   private
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.before_eager_load do
+    Rails.application.reload_routes!
+  end
+  
   config.force_ssl = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
